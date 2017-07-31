856bb35fb398aad5822ab3cb9318b883fb36435e
Michael Clayton <mclayton@redhat.com>
2017-07-30 23:21:00 -0400

    update in orange light

--- source/in-orange-light/js/config.js
+++ source/in-orange-light/js/config.js
@@ -12,7 +12,7 @@ var config = Object.freeze({
     SIM_UPDATE_FREQUENCY: 1 * Phaser.Timer.SECOND,
 
     // win after surviving this long
-    WIN_TIME: 90,
+    WIN_TIME: 80,
 
     INITIAL_STATE: {
         alive: true,
